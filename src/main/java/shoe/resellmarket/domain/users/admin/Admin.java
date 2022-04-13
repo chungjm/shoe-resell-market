f965039654beb1890d855b5e917cc7382f815d5e
Jongmin <chungjm0711@gmail.com>
2022-04-13 16:45:07 +0900

    git admin-domain commit

--- src/main/java/shoe/resellmarket/domain/users/admin/Admin.java
+++ src/main/java/shoe/resellmarket/domain/users/admin/Admin.java
@@ -1,8 +1,10 @@
 package shoe.resellmarket.domain.users.admin;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import shoe.resellmarket.domain.users.common.UserBaseEntity;
+import shoe.resellmarket.domain.users.common.UserLevel;
 
 import javax.persistence.Entity;
 
@@ -10,4 +12,10 @@ import javax.persistence.Entity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Admin extends UserBaseEntity {
 
+    @Builder
+    public Admin(String email, String password, UserLevel userLevel) {
+        this.email = email;
+        this.password = password;
+        this.userLevel = userLevel;
+    }
 }
---
+++ src/main/java/shoe/resellmarket/domain/users/admin/AdminRepository.java
@@ -0,0 +1,6 @@
+package shoe.resellmarket.domain.users.admin;
+
+
+public interface AdminRepository {
+
+}
