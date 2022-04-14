6c8b304912292781a9d02b7d77054c74b6ad3ef0
Jongmin <chungjm0711@gmail.com>
2022-04-15 00:28:27 +0900

    trade-domain 생성

--- src/main/java/shoe/resellmarket/domain/trade/Trade.java
+++ src/main/java/shoe/resellmarket/domain/trade/Trade.java
@@ -1,9 +1,11 @@
 package shoe.resellmarket.domain.trade;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import shoe.resellmarket.domain.BaseTimeEntity;
+import shoe.resellmarket.domain.address.Address;
 import shoe.resellmarket.domain.product.Product;
 import shoe.resellmarket.domain.users.user.User;
 
@@ -40,5 +42,35 @@ public class Trade extends BaseTimeEntity {
 
     private double productSize;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "return_id")
+    private Address shippingAddress;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shipping_id")
+    private Address returnAddress;
+
+    private String receivingTrackingNumber;
+
+    private String forwardingTrackingNumber;
+
+    private String returnTrackingNumber;
+
+    private String cancelReason;
+
+    @Builder
+    public Trade(Long id, User publisher, User seller, User buyer,
+                 Product product, TradeStatus status, Long price, double productSize,
+                 Address shippingAddress, Address returnAddress) {
+        this.id = id;
+        this.publisher = publisher;
+        this.seller = seller;
+        this.buyer = buyer;
+        this.product = product;
+        this.status = status;
+        this.price = price;
+        this.productSize = productSize;
+        this.shippingAddress = shippingAddress;
+        this.returnAddress = returnAddress;
+    }
 }
--- src/main/java/shoe/resellmarket/domain/trade/TradeStatus.java
+++ src/main/java/shoe/resellmarket/domain/trade/TradeStatus.java
@@ -1,5 +1,17 @@
 package shoe.resellmarket.domain.trade;
 
+/**
+ * PRE_CONCLUSION: 거래 체결전
+ * PRE_SELLER_SHIPMENT: 판매자 발송 대기
+ * PRE_WAREHOUSING: 입고 대기(판매자 -> 회사)
+ * INSPECTION: 검수
+ * PRE_SHIPMENT: 검수후 발송 대기
+ * SHIPPING: 배송중(회사 -> 구매자)
+ * COMPLETE: 거래 완료
+ * CANCEL: 취소(판매자가 기간 내 상품 미발송 및 검수 -> 부적합)
+ */
 public enum TradeStatus {
-
+    PRE_CONCLUSION, PRE_SELLER_SHIPMENT, PRE_WAREHOUSING,
+    INSPECTION, PRE_SHIPMENT, SHIPPING,
+    COMPLETE, CANCEL,
 }
